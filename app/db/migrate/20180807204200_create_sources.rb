c8154d7fd9616bdabbf7531216035bb838bb8bf3
booty <johnedmundrose@gmail.com>
2018-08-08 13:47:33 -0400

    Seeds.rb code to get sources into the database

--- app/db/migrate/20180807204200_create_sources.rb
+++ app/db/migrate/20180807204200_create_sources.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CreateSources < ActiveRecord::Migration[5.2]
   def change
     create_table :sources do |t|
-    	t.string :name, null: false
-    	t.string :url, null: false
-    	t.boolean :active, null: false, default: true
-    	t.boolean :real, null: false
-    	t.timestamps
+      t.string :name, null: false
+      t.string :url, null: false
+      t.boolean :active, null: false, default: true
+      t.boolean :real, null: false
+      t.timestamps
     end
   end
 end
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sources = [
+  {
+    name: 'NBC New York',
+    url: 'https://www.nbcnewyork.com/news/weird/?rss=y',
+    real: true
+  },
+  {
+    name: 'New York Post',
+    url: 'https://nypost.com/tag/weird-but-true/feed/',
+    real: true
+  },
+  {
+    name: 'National Public Radio',
+    url: 'https://www.npr.org/rss/rss.php?id=1146',
+    real: true
+  },
+  {
+    name: 'The Onion',
+    url: 'https://www.theonion.com/rss',
+    real: false
+  },
+  {
+    name: 'The Onion: Politics',
+    url: 'https://politics.theonion.com/rss',
+    real: false
+  }
+]
+
+sources.each do |source|
+  Source.create(source)
+end
